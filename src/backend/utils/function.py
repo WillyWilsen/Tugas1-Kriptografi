fac2ec25187a89c1c2122167bd8e6a89423bd620
Willy Wilsen <13520160@std.stei.itb.ac.id>
2024-02-21 16:28:22 +0700

    Final Source

--- src/backend/utils/function.py
+++ src/backend/utils/function.py
@@ -1,3 +1,3 @@
-# Clean text from number, space, & special characters and convert to lower case
+# Clean text from number, space, & special characters and convert to upper case
 def clean_text(text):
-  return ''.join(e for e in text if e.isalnum()).upper()
+  return ''.join(e for e in text if e.isalpha()).upper()
